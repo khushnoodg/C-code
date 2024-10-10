f3a7a82c971e3c20db571639acff615f0be75170
Khushnood <khushnood.contact@gmail.com>
2024-10-10 09:52:47 +0500

    signup added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <signup.cpp>
 using namespace std;
 int main(){
     cout<<"Hello World"<<endl;
     cout<<"second line of the main program"
+    signup()
     return 0;
     _getch();
 }
